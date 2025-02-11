58c5c90cbcfd5e8e4fb7ec3c1177a997c849221a
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2025-02-11 18:07:58 -0500

    Added script to configure ZFS kernel options

---
+++ formatting/configure-zfs.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+## Configure ZFS kernel settings
+
+## Make sure we're root
+if [[ $EUID -ne 0 ]]; then
+  echo "ERROR: This script must be run as root." >&2
+  exit 1
+fi
+
+## Get environment variables
+ENV_FILE='./env.sh'; if [[ -f "$ENV_FILE" ]]; then source ./env.sh; else echo "ERROR: Missing '$ENV_FILE'."; exit -1; fi
+if [[ \
+    -z "$ENV_ACCEPTABLE_DATA_LOSS_SECONDS" ||\
+    -z "$ENV_SLOWEST_HDD_MAX_SPEED_MBPS" \
+]]; then
+  echo "ERROR: Missing environment variables." >&2
+  exit 2
+fi
+
+## Recreate the config file that this script manages.
+FILE='/etc/modprobe.d/zfs-customized.conf'
+: > "$FILE"
+chmod 644 "$FILE"
+
+## Write ZFS configurations to the file.
+echo "options zfs zfs_txg_timeout=$ENV_ACCEPTABLE_DATA_LOSS_SECONDS" >> "$FILE"
+echo "options zfs zfs_txg_size_limit=$(($ENV_SLOWEST_HDD_MAX_SPEED_MBPS * $ENV_ACCEPTABLE_DATA_LOSS_SECONDS))" >> "$FILE"
+echo "options zfs zfs_immed_write_size=0" >> "$FILE"
+
+## Notify user and exit.
+echo "Please reboot for these settings to take effect."
+exit 0
