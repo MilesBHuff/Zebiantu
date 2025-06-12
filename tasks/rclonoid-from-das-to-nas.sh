293d36faa580ae008a24f498f47b829b22e573c0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2025-06-12 10:13:27 -0400

    Added another option for `rclonoid`ing

--- tasks/rclonoid-from-das-to-nas.sh
+++ tasks/rclonoid-from-das-to-nas.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-#rclonoid /media/zfs/das-pool/nas-pool/legacy/ /media/zfs/nas-pool/legacy/
-rclonoid /media/zfs/das-pool/nas-pool/legacy/recovered/ /media/zfs/nas-pool/legacy/recovered/
+# rclonoid /media/zfs/das-pool/nas-pool/legacy/ /media/zfs/nas-pool/legacy/
+# rclonoid /media/zfs/das-pool/nas-pool/legacy/recovered/ /media/zfs/nas-pool/legacy/recovered/
+# rclonoid /media/zfs/das-pool/nas-pool/legacy/recovered/ /media/zfs/nas-pool/recovered/
