3d0b7698cc8b292a2466fc1e8552afd2031b7b06
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-02-13 00:14:42 -0500

    Managed to eliminate the pagevol from the plan.

--- setup/3-initial-installation/modules/3-boot/4-hibernation.bash
+++ setup/3-initial-installation/modules/3-boot/4-hibernation.bash
@@ -4,24 +4,24 @@
 ## > Hibernation is a useful but universally overlooked feature in the server space.
 ## > In the event of a prolonged power-outage, a system connected to a UPS can hibernate instead of powering off.
 ## > At the end of the outage, this system can then restore to its prior state.
-## > This is much faster and much-less-disruptive than a true cold boot, and crucially for some ZFS setups: it means you don't have to rebuild L2ARC.
+## > This is much faster and much-less-disruptive than a true cold boot.
 ## > Hibernation is also, obviously, very useful on a laptop: The system can hibernate before the battery dies, thus allowing resumption without a reboot once power is resupplied.
+## > That means you no longer ever have to worry about finishing up and closing everything before your laptop dies.
 ##
 ## **Etymology:**
 ## > Windows, unlike Linux, properly distinguishes the roles of the on-disk memory cache ("pagefile") and hibernation cache ("hiberfile").
 ## > This script introduces that distinction to Linux.
-## > As swap devices in a ZFS system are nigh-necessarily zvols, I have taken to calling the former a "pagevol" and the latter a "hibervol", by analogy.
+## > As swap devices in a ZFS system are nigh-necessarily zvols, I have taken to calling ours a "hibervol", by analogy.
 ##
 ## **Hibernation:**
-## > Create a new sparse zvol with snapshots disabled and compression enabled (using the same algorithm as zram swap: zstd-2), equal to the size of the zram swap: "pagevol" (Why: We need somewhere on-disk to dump the contents of zram swap)
-## > Create a new sparse zvol with snapshots disabled and compression disabled: "hibervol" (Why: The Linux kernel already compresses the memory sent to the hibervol.)
-## > Format pagevol as swap with name "pageswap" and priority `-1`. (the lowest possible)
-## > Format hibervol as swap with name "hiberswap" and priority `100` (an arbitrary number that is higher than pageswap and lower than zram swap).
-## > Swapon pageswap, then drop unneeded caches (`vm.drop_caches=3`), then disable systemd-oomd, then swapoff zram swap, then disable zswap.
-## > * If free RAM is limited, this will temporarily cause a substantial drop in performance as the kernel moves things from zram swap to pageswap. Expect lockups and potentially thrashing if zram swap is substantial.
-## > * Dropping unneeded caches beforehand should help a lot, but it's no guarantee that there will not be a serious performance hit.
+## > Trigger `sync` (to free up dirty write caches), then drop unneeded caches (`vm.drop_caches=3`), then wait 5 seconds (an arbitrary figure; heuristically set to be coincident with `vm.dirty_writeback_centisecs`).
+## > Create a new sparse zvol named "hibervol", with snapshots disabled and compression enabled (using the same algorithm as zram swap: zstd-2). It should be equal to the size of zram swap + zswap + used RAM.
+## > Format hibervol as swap with name "hiberswap" and priority `-1`. (the lowest possible)
+## > Swapon hiberswap, then disable systemd-oomd, then swapoff zram swap, then disable zswap.
+## > * If free RAM is limited, this will temporarily cause a substantial drop in performance as the kernel moves things from zram swap to hiberswap. Expect lockups and potentially thrashing if zram swap is substantial.
 ## > * If memory pressure is sufficiently severe, an OOM killer could be engaged. It's imperative that we avoid that eventuality. We can disable systemd's, but we can't disable the kernel's.
-## > Compact memory (`vm.compact_memory=1``), swapon hibervol and initiate hibernation.
+## > Compact memory (`vm.compact_memory=1`), then disable compression on hibervol, then `zpool sync`, then initiate hibernation.
+## > * The kernel has its own compression algorithm for hibernation; ergo, ZFS compression should be disabled, lest we double-compress.
 ## > * Memory compaction is optional, but it may result in a higher compression ratio; and, in any case, it will help with performance after resume.
 ##
 ## **Restoration:**
@@ -30,20 +30,21 @@
 ## > If hibervol is not present, initramfs loads the system normally.
 ## > If hibervol is present, initramfs resumes from it.
 ## > If an error is encountered, the system attempts
-## > After restoration: enable zswap, then swapon zram swap, then swapoff hiberswap, then swapoff pageswap.
-## > After swapoffs finish: enable systemd-oomd, delete hibervol, then delete pagevol.
+## > After restoration: enable zswap, then swapon zram swap, then swapoff hiberswap.
+## > After swapoff finishes: enable systemd-oomd, then delete hibervol.
 ##
 ## **Failsafe:**
-## > We check for the existence of the pagevol and the hibervol on normal boots.
-## > If found, we delete them and log a warning.
+## > We check for the existence of the hibervol on normal boots.
+## > If found, we delete it and log a warning.
 ## > This helps ensure clean operation even in the event that something ever goes wrong with hibernation.
 ##
 ## **Contingency:**
 ## > Set a hard quota on the OS zpool equal to the total amount of installed RAM.
-## > This should virtually ensure that there is always room for a hibervol to be made.
-## > How:
-## > * We move zram swap (max 1/3 of RAM) to a swap zvol with the same size and compression algorithm. So to guarantee that we can fit the contents of zram swap, we only need to reserve 1/3 of total RAM on the storage pool.
-## > * The other 2/3 of RAM is uncompressed (1:1, if unswapped) or lightly compressed (2:1, if zswapped). Linux typically compresses to roughly about 5:2 when hibernating, which is at least as much as zswap is doing; so a reservation of 2/3 total RAM is a guarantee that we will always have enough space to hibernate.
+## > This naïve 1:1 reservation guarantees solvency because:
+## > * zram swap is drained from zstd-2 to zstd-2 (the same algorithm).
+## > * zswap is drained from lz4 to zstd-2 (a stronger algorithm).
+## > * The rest of RAM is hibernated from uncompressed to kernel compression (a stronger algorithm).
+## > * RAM is never at 100% use; the kernel doesn't permit it.
 ##
 ## **Whys:**
 ## > Why bother with this? Why not just have permanent on-disk swap all the time? Why not just not have swap at all?
@@ -72,11 +73,6 @@
 ## > * If resume fails, you may be unable to boot without manual intervention.
 ## > * If memory pressure spikes too high during swap drains, an OOM killer could be triggered.
 ## > My goal is to eliminate these risks.
-##
-## **Theory TODOs:**
-## > zram swap is just a zram device formatted as a swap partition; because of this, I *assume* that it is excluded from hibernation just as any other swap device would be, and that it must therefore be drained to durable storage prior to hibernation.
-## > However, I don't actually know for certain that this caveat is true; it is possible that perhaps the kernel hibernates the entirety of RAM *period*, without regard to whether some of RAM is a swap device. It is also possible that the kernel may treat zram swap differently from normal swap.
-## > I need to confirm before beginning implementation.
 
 #TODO: Implement the above.
 #TODO: Enable automatic hibernation when NUT detects that the UPS is low on battery.
