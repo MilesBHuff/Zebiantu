038d8297ab198643817a8dba1d1453d4ff01c19e
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-02-18 13:56:36 -0500

    Remove Sanoid when converting to Proxmox

--- setup/5-conversion/convert-debian-to-proxmox.bash
+++ setup/5-conversion/convert-debian-to-proxmox.bash
@@ -17,3 +17,8 @@ source "$SCRIPT"
 rm "$SCRIPT"
 unset SCRIPT
 #TODO: I don't really agree with all the decisions the writer of the above script made, and would accordingly like to have my own conversion script.
+
+## Proxmox has its own snapshotting functionality; we need to use that instead of Sanoid.
+apt purge -y sanoid
+rm -f '/etc/sanoid/sanoid.conf' || true
+echo 'You need to manually set up snapshotting inside of Proxmox.'
