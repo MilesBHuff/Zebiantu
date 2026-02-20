76cbe1181f3491c972a585665fb1dfcd066f7814
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-02-20 17:53:00 -0500

    Configured chrony.

--- ReadMe.md
+++ ReadMe.md
@@ -60,7 +60,7 @@ Scripts that install an operating system to a ZFS root. These scripts are capabl
         * `hibernation`: Disables stock hibernation/resume, then optionally **allows hibernation by way of temporary swap zvol** if the system uses dracut to build its initramfs.
     * `apps`: Add and configure various applications.
         * `packages`: Install all sorts of things that the system will need.
-        * `time`: [WIP] Configure the system's time daemon.
+        * `time`: Configure the system's time daemon.
         * `font`: [WIP] Give the system a better text-mode font.
         * `tty`: Configures tty9 to run `btop` and tty10 to display the system console; and adds an easy way to display VM consoles on tty11+.
         * `integrity`: [WIP] Configures some automatic integrity checks.
--- setup/3-initial-installation/modules/4-apps/2-time.bash
+++ setup/3-initial-installation/modules/4-apps/2-time.bash
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 #NOTE: This script is a fragment sourced by a parent script running in a `chroot`.
-#TODO: Configure Chrony
+
+## See here for documentation: https://github.com/MilesBHuff/NixOS/blob/master/persist/etc/chrony/chrony.conf
+cat > '/etc/chrony/chrony.conf' << 'EOF'
+pool time.cloudflare.com nts iburst
+# minsources 3
+dumpdir     /var/lib/chrony/dump/
+logdir      /var/log/chrony/logs/
+ntsdumpdir  /var/lib/chrony/ntsdump/
+driftfile   /var/lib/chrony/drift
+# hwclockfile /etc/adjtime
+rtcfile     /var/lib/chrony/rtc
+dumponexit
+lock_all
+nocerttimecheck 1
+rtcautotrim 1
+makestep 0.1 4
+rtconutc
+leapsectz UTC
+hwtimestamp *
+maxdistance 1.5
+maxupdateskew 250
+EOF
+echo 'You should manually replace `time.cloudflare.com` with your local timeserver and change `pool` to `server`.'
+echo 'If your local timeserver does not support NTS, make sure you also change `nts` to `ntp`.'
+echo 'I recommend configuring your local timeserver to smear leap seconds; else, you risk occasional random Y2K events until 2035.'
