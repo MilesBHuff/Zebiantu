bbf204a0916ac7082942e21b7a68e0401db70871
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-02-25 13:01:46 -0500

    Add wrapper code for the upstream Proxmox conversion script.

--- setup/5-conversion/convert-debian-to-proxmox.bash
+++ setup/5-conversion/convert-debian-to-proxmox.bash
@@ -7,18 +7,33 @@ function helptext {
     echo 'Luckily for me, someone else already went through the trouble of making this, so this script just calls theirs.'
 }
 ## Instructions: https://pve.proxmox.com/wiki/Install_Proxmox_VE_on_Debian_13_Trixie
+#NOTE: I don't really agree with all the decisions the writer of the upstream script made, so we have to override it in a few spots.
+
+## Proxmox has its own snapshotting functionality; we need to use that instead of Sanoid.
+apt purge -y sanoid
+rm -f '/etc/sanoid/sanoid.conf' || true
+echo 'You need to manually set up snapshotting inside of Proxmox.'
+
+## We need to modify, back up, and later restore our current sources.list
+cp -a '/etc/apt/sources.list' '/etc/apt/sources.list.bak'
+cat >> '/etc/apt/sources.list.bak' <<'EOF'
+
+deb      https://download.proxmox.com/debian/pve       trixie                            pve-no-subscription
+EOF
 
 ## Call an upstream script that does it all for us.
 SCRIPT=$(mktemp)
 curl -O https://raw.githubusercontent.com/MrMasterbay/proxmox-from-scratch/main/little-goblin.sh "$SCRIPT"
 chmod +x "$SCRIPT"
 # exec "$SCRIPT"
+read -r 'We will now execute the conversion script. When it completes, do NOT allow it to restart your system; we need to run some things after it finishes.'
 source "$SCRIPT"
 rm "$SCRIPT"
 unset SCRIPT
-#TODO: I don't really agree with all the decisions the writer of the above script made, and would accordingly like to have my own conversion script.
 
-## Proxmox has its own snapshotting functionality; we need to use that instead of Sanoid.
-apt purge -y sanoid
-rm -f '/etc/sanoid/sanoid.conf' || true
-echo 'You need to manually set up snapshotting inside of Proxmox.'
+## Restore custom sources.list
+mv -f '/etc/apt/sources.list.bak' '/etc/apt/sources.list'
+apt update -y
+
+## Done
+exit 0
