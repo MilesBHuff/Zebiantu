a37cb41ff337c0a7423fca346e09c462987d7373
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-02-25 10:32:21 -0500

    Added notes and a todo

--- setup/3-initial-installation/modules/4-apps/5-integrity.bash
+++ setup/3-initial-installation/modules/4-apps/5-integrity.bash
@@ -4,4 +4,9 @@ apt install -y \
     clamav \
     debsums \
     rkhunter
-#TODO
+
+#NOTE: ClamAV scans are basically scrubs in terms of intensity, so they should be run only very sparingly. It's probably best to find some way to only run them on new I/O — maybe every time a file is written to, it is added to a "to scan" list, and ClamAV then gradually scans that list when the system is idle.
+#NOTE: debsums is of only moderate utility — ZFS checksums prevent latent corruption, debsums has false positives resulting from normal system use, and any attacks found this way are likely to indicate root-level exploits, which if present means we've already lost.
+#NOTE: rkhunter is of relatively limited efficacy nowadays, but it doesn't hurt to have it.
+
+#TODO: Configure ClamAV
